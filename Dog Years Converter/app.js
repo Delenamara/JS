69d7faf8536c2c34640f67aae70043405d02da97
Margarita <delenamara.prohorova@gmail.com>
2020-07-20 12:32:40 +0300

    Update app.js

--- Dog Years Converter/app.js
+++ Dog Years Converter/app.js
@@ -1,19 +1,13 @@
-//creating a variable and setting it to my current age
+const myName = 'Rita';
 const myAge = 20;
-//creating a variable earlyYears
 let earlyYears = 2;
-earlyYears *= 10.5;
-//18 years left
 let laterYears = myAge - 2;
-//18 years in dog years
+
+earlyYears *= 10.5;
 laterYears *= 4;
 console.log(earlyYears);
 console.log(laterYears);
 
-//20 years in dog years
 myAgeInDogYears = earlyYears + laterYears;
-//string to lowercase
-let myName = 'Rita';
 myName = myName.toLowerCase();
-//creating a full text for the output
 console.log('My name is ' + myName + '. I am ' + myAge + ' years old in human years which is ' + myAgeInDogYears + ' years old in dog years.');
