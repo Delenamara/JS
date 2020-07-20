2945d43f40424e453269fc5f110864ff659d43cf
Margarita <delenamara.prohorova@gmail.com>
2020-07-20 12:24:30 +0300

    Update app.js

--- Race Day/app.js
+++ Race Day/app.js
@@ -1,19 +1,17 @@
-//creating a variable and setting it to my current age
-const myAge = 20;
-//creating a variable earlyYears
-let earlyYears = 2;
-earlyYears *= 10.5;
-//18 years left
-let laterYears = myAge - 2;
-//18 years in dog years
-laterYears *= 4;
-console.log(earlyYears);
-console.log(laterYears);
+let raceNumber = Math.floor(Math.random() * 1000);
+let isRegisteredEarly = true;
+let age = 20;
 
-//20 years in dog years
-myAgeInDogYears = earlyYears + laterYears;
-//string to lowercase
-let myName = 'Rita';
-myName = myName.toLowerCase();
-//creating a full text for the output
-console.log('My name is ' + myName + '. I am ' + myAge + ' years old in human years which is ' + myAgeInDogYears + ' years old in dog years.');
+if (age > 18 && isRegisteredEarly) {
+    raceNumber += 1000;
+}
+
+if (age > 18 && isRegisteredEarly) {
+    console.log('Your race will start at 9:30 am.\nYour race number is: ' + raceNumber);
+} else if (age > 18 && isRegisteredEarly === false) {
+    console.log('Your race will start at 11:00 am.\nYour race number is: ' + raceNumber);
+} else if (age < 18) {
+    console.log('Your race will start at 12:30 am.\nYour race number is: ' + raceNumber);
+} else if (age === 18) {
+    console.log('You need to see the registration desk.')
+}
